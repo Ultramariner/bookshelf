cf040124f8e712262e1997a78e5553c650421aae
Ultramar <artyom.odinochenko@yandex.ru>
2024-07-08 00:38:41 +0300

    Dispatcher servlet

---
+++ src/main/java/org/myprojects/bookshelf/WebConfig.java
@@ -0,0 +1,23 @@
+package org.myprojects.bookshelf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+//@EnableWebMvc
+//@ComponentScan("org.myprojects")
+public class WebConfig {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+}
---
+++ src/main/java/org/myprojects/bookshelf/WebMvcInitializer.java
@@ -0,0 +1,22 @@
+package org.myprojects.bookshelf;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRegistration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+@Configuration
+public class WebMvcInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.scan("org.myProjects");
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcher", new DispatcherServlet(context));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+    }
+}
